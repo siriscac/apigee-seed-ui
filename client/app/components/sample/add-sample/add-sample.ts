372d437460de66c58e93919befec26c8914a9852
mukundha@apigee.com <mukundha@apigee.com>
2016-09-01 15:23:49 -0700

    display name

--- client/app/components/sample/add-sample/add-sample.ts
+++ client/app/components/sample/add-sample/add-sample.ts
@@ -69,7 +69,8 @@ export class AddSampleComponent {
     save() {
         var data: any = this.sampleForm.value;
         this.show_spinner = true;
-        this.sample = new Sample('', '', data.name, data.description, '', data.gitURL, data.apiFolder, this.authService.getUserInfo(), '');
+        
+        this.sample = new Sample('', '' , data.name,  data.description, '', data.gitURL, data.apiFolder, this.authService.getUserInfo(), '');
 
         this.sampleService.genericCallback = (error, data) => {
             if (data) {
